f63f4a155a34fea830709fb06c6fd0f002998fda
Gerald Strom <gstrom47@gerald-stroms-computer-2.local>
2014-03-28 16:46:31 -0500

    Initial Commit

---
+++ HelloWorld/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Gerald Strom on 3/28/14.
+//  Copyright (c) 2014 Gerald Strom. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
